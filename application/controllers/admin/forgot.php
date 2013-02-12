0b8204b1681b0723fd805853f8e843610b961dc0
joe <joe@tuxmobile>
2013-02-12 10:53:16 +0800

    Change db and output method

--- application/controllers/admin/forgot.php
+++ application/controllers/admin/forgot.php
@@ -2,6 +2,8 @@
 class Forgot extends CI_Controller {
     public function __construct() {
         parent::__construct();
+        $this->load->model("Usersq","uq");
+        $this->load->library('email');
     }
     
     public function index() {
@@ -18,7 +20,7 @@ class Forgot extends CI_Controller {
         }
         
         $data['hash'] = $this->session->userdata('hash_value');
-        $this->load->view("forgot_view",$data);
+        $this->load->view("admin/forgot_view",$data);
     }
     
     public function resend() {
@@ -30,39 +32,28 @@ class Forgot extends CI_Controller {
         $email = $this->input->post('email');
         
         if($csrf_val === $csrf_session && ! empty($email)) {
-            
-            $token = md5(uniqid(rand(), true));
-            $content = file_get_contents(FCPATH.'templates/forgot.txt');
-            $confirmation_link = base_url().'forgot/confirm/'.$token;
-            $mailed = FALSE;
-            
-            $sql = $this->db->query("
-                                        SELECT a.username, b.* FROM pre_users a, pre_profile b
-                                        WHERE
-                                        a.id=b.id AND b.email=".$this->db->escape($email)." AND a.confirmation='1'
-            ");
-            $num_res = $sql->num_rows();
-            
-            if($num_res === 1) {
+            if($this->uq->is_exist($email) >= 1) {
                 
-                $result = $sql->result();
+                $token = md5(uniqid(rand(), true));
+                $content = file_get_contents(FCPATH.'templates/forgot.txt');
+                $confirmation_link = base_url().'forgot/confirm/'.$token;
+                $mailed = FALSE;
                 
+                $result = $this->uq->fetch_confirmed_user_profile_by_email($email);
                 $array = array(
-                                "id"    =>  $result[0]->id,
-                                "token"  =>  $token,
-                                "date"  =>  date('Y-m-d H:i:s'),
-                                "source"=>  "forgot"  
+                                "id"        =>  $result[0]->id,
+                                "token"     =>  $token,
+                                "date"      =>  date('Y-m-d H:i:s'),
+                                "source"    =>  "forgot"  
                 );
                 
-                $this->db->insert("pre_tokens", $array);
-                $aff_rows = $this->db->affected_rows();
-                
-                if($aff_rows === 1) {
-                    
+                $aff_rows = $this->uq->generate_token($array);
+
+                if($aff_rows >= 1) { 
                     $content = str_replace("##USERNAME##", $result[0]->username, $content);
                     $content = str_replace("##HOST##", base_host(), $content);
                     $content = str_replace("##CONFIRM_LINK##", $confirmation_link, $content);
-
+                                       
                     $config['mailtype'] = 'html';
                     $this->email->initialize($config);
 
@@ -70,52 +61,40 @@ class Forgot extends CI_Controller {
                     $this->email->to($result[0]->email); 
                     $this->email->subject('Password Recovery');
                     $this->email->message($content);	
-
+                    
                     if($this->email->send()) {
                         $array = array('status'=>1,'message'=>"Please check your email");
-                        $this->session->set_userdata($array);
+                        $this->session->set_flashdata($array);
                     } else {
                         $array = array('status'=>0,'message'=>"Failed to send the password confirmation link");
-                        $this->session->set_userdata($array);
+                        $this->session->set_flashdata($array);
                     }
                     
                 } else {
                     
                     $array = array('status'=>0,'message'=>"Failed to send the password confirmation link");
-                    $this->session->set_userdata($array);
+                    $this->session->set_flashdata($array);
                 }
                 
             } else { 
                 $array = array('status'=>0,'message'=>"Email does not exist / Un-confirmed");
-                $this->session->set_userdata($array);
+                $this->session->set_flashdata($array);
             }
             
         } else {
             $array = array('status'=>0,'message'=>"Invalid session");
-            $this->session->set_userdata($array);
+            $this->session->set_flashdata($array);
         }
-        redirect(base_url()."forgot","location");
+        redirect(base_url()."admin/forgot","location");
     }
     
     public function confirm() {
-        
-        $this->load->library("Mlib_sec");
         
         $token = $this->uri->segment(3);
 
         if(! empty($token)) {
             
-            $sql = $this->db->query("
-                                        SELECT a.*,b.email from pre_tokens a, pre_profile b
-                                        WHERE 
-                                        a.token=".$this->db->escape($token)." AND a.visited='0' AND a.date > DATE_SUB(NOW(),INTERVAL 24 HOUR) 
-                                            AND a.source='forgot' AND b.id=a.id
-            ");        
-            
-            $num_res = $sql->num_rows();
-            
-            
-            if($num_res === 1) {
+            if($this->uq->is_valid_token($token) === 1) {
                 
                 $this->load->library("Mlib_sec");
                 
@@ -197,7 +176,7 @@ class Forgot extends CI_Controller {
             $array = array('status'=>0,'message'=>"Direct access forbidden");
             $this->session->set_userdata($array);
             
-            redirect(base_url()."forgot","location");
+            redirect(base_url()."admin/forgot","location");
         }
     }
     
